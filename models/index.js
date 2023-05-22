7d5031b08ad0f30ad82d8891270e3e4e8ac24b84
Austin1094 <Austin.Adams1094@gmail.com>
2023-05-21 21:58:06 -0400

    Updated index.js so that Categories has many Products

--- models/index.js
+++ models/index.js
@@ -10,6 +10,9 @@ const ProductTag = require('./ProductTag');
 });
 
 // Categories have many Products
+ Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
 
